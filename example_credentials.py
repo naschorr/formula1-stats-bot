1876a78236d9a162ac3e91f8def23d44197e5320
naschorr <naschorr@gmail.com>
2015-01-18 23:50:44 -0500

    Added example credentials file

---
+++ example_credentials.py
@@ -0,0 +1,16 @@
+DATABASE = "database"
+HOSTNAME = "host"
+USERNAME = "user"
+PASSWORD = "password"
+
+def database():
+	return DATABASE
+
+def hostname():
+	return HOSTNAME
+
+def username():
+	return USERNAME
+
+def password():
+	return PASSWORD
