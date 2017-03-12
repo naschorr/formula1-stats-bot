f47becc77b107b2b023fbb2abce77065b5fa599b
Nick Schorr <naschorr@gmail.com>
2017-03-12 00:33:01 -0500

    start-scraper is now more cron friendly
    - It now works when set to run on @reboot, and doesn't need postgres to be started before invocation

--- start-scraper.sh
+++ start-scraper.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
 
-source bin/activate
-python code/scraper.py
+POSTGRES="/usr/sbin/service postgresql status"
+POSTGRES_VERSION="9.1"
+
+function postgres_started {
+	status="$(eval $POSTGRES)"
+	if [ "$(eval grep $POSTGRES_VERSION <<< $status)" ]; then
+		return 0
+	else
+		return 1
+	fi
+}
+
+SLEEP_TIME=5
+
+until postgres_started; do
+	echo "$(POSTGRES_STATUS)"
+	echo "Postgres not currently running, trying again in $SLEEP_TIME."
+	sleep $SLEEP_TIME
+done
+
+# http://stackoverflow.com/a/246128
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+source $DIR/bin/activate
+python $DIR/code/scraper.py
+deactivate
