2e3458231d88636f3384a81906f0df810b017998
Nick Schorr <naschorr@gmail.com>
2017-03-11 20:58:14 -0500

    Fixed install-requirements.hs, renamed requirements.txt

--- install-requirements.sh
+++ install-requirements.sh
@@ -4,13 +4,11 @@ TEMP_REQS="temp_reqs.txt"
 
 source bin/activate
 pip install -r base-requirements.txt
-pip freeze > $TEMP_REQS
 echo "Attempting to pip install psycopg2cffi"
 pip install psycopg2cffi
-if grep -Fxq "psycopg2cffi" $TEMP_REQS
+pip freeze > $TEMP_REQS
+if ! grep -Fq "psycopg2cffi" $TEMP_REQS
 then
-	exit 1
-else
 	echo "Attempting to install psycopg2 instead"
 	pip install psycopg2
 fi
