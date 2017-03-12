a3625b68543b413506c384886930228e875d5613
Nick Schorr <naschorr@gmail.com>
2017-03-12 19:47:57 -0400

    Moved the db_controller out into its own script

--- code/scraper.py
+++ code/scraper.py
@@ -9,16 +9,8 @@ import requests
 
 from time import sleep
 
-if(os.name == "posix"):
-    ## https://mail.python.org/pipermail/pypy-dev/2013-May/011398.html
-    import psycopg2cffi as psycopg2
-elif(os.name == "nt"):
-    import psycopg2
-else:
-    ## No idea what os it is, just try psycopg2
-    import psycopg2
-
 from comment import Comment
+from db_controller import DB_Controller
 
 ## Globals
 CONFIG_FOLDER_NAME = "config"
@@ -30,72 +22,6 @@ SLEEP_TIME = 10
 
 DB_PATH = os.path.sep.join(os.path.realpath(__file__).split(os.path.sep)[:-2] + [CONFIG_FOLDER_NAME, DB_CONFIG_NAME])
 REDDIT_PATH = os.path.sep.join(os.path.realpath(__file__).split(os.path.sep)[:-2] + [CONFIG_FOLDER_NAME, REDDIT_CONFIG_NAME])
-
-class DB_Controller:
-    def __init__(self, db_cfg_path):
-        with open(db_cfg_path) as db_json:
-            self.db_cfg = json.load(db_json)
-
-        try:
-            self.db = psycopg2.connect(database=self.db_cfg["database"],
-                                       host=self.db_cfg["hostname"],
-                                       user=self.db_cfg["username"],
-                                       password=self.db_cfg["password"])
-        except psycopg2.OperationalError as poe:
-            print("Unable to connect to database:", poe, file=sys.stderr)
-            sys.exit()
-        except:
-            print("Unhandled error when connecting to database", sys.exc_info()[1], file=sys.stderr)
-            sys.exit()
-
-        self.table = self.db_cfg["table"]
-
-        print("Currently {0} rows in table {1}.".format(self.count_rows(), self.table))
-
-
-    def count_rows(self):
-        with self.db.cursor() as cursor:
-            cursor.execute("SELECT COUNT(*) FROM {0};".format(self.table))
-            return cursor.fetchone()[0]
-
-
-    def store_comment(self, comment_obj):
-        ## Stage changes to the db
-        with self.db.cursor() as cursor:
-            raw =  """INSERT INTO {0} (post_id, author, time_created, flair, 
-                      body) VALUES (%s, %s, %s, %s, %s);"""
-            try:
-                cursor.execute(raw.format(self.table), (comment_obj.id.id,
-                                                        comment_obj.author,
-                                                        comment_obj.time,
-                                                        comment_obj.flair,
-                                                        comment_obj.text))
-            except psycopg2.IntegrityError as pie:
-                print("Primary Key integrity violation. Ignoring this comment.", pie, file=sys.stderr)
-                self.db.rollback()
-            except:
-                print("Unhandled error when inserting into db", sys.exc_info(), file=sys.stderr)
-                sys.exit()
-            else:
-
-                ## Commit changes to the db
-                try:
-                    self.db.commit()
-                except:
-                    print("Unhandled error when commiting changes to db", sys.exc_info(), file=sys.stderr)
-                    sys.exit()
-                else:
-
-                    ## Output the successfully added comment
-                    self.dump_comment(comment_obj)
-
-
-    def dump_comment(self, comment_obj):
-        try:
-            comment_obj.print_all()
-            sys.stdout.flush()
-        except UnicodeEncodeError as uee:
-            print("Error encoding a character", uee, "\nSkipping...")
 
 
 class Scraper:
